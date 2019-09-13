0d6b56f9bec9941114b921a880560195bb8d15c7
respawn_sugiyama <sugiyama@respawn.tokyo>
2019-09-13 17:49:35 +0900

    learn props of compornent

--- src/App.js
+++ src/App.js
@@ -7,13 +7,35 @@ import React from 'react';
 //classはJSの予約語なのでJSXではclassNameと書く
 //属性名等は全てキャメルケースでかく。
 //React.FragmentはHTML上でみると表示されなくなるタグ。JSXで記述するときは親タグをつけなくてはいけないので、それをHTML上に表示させないようにするタグ。
-export default class App extends React.Component {
-  render() {
-      return (
-        <React.Fragment>
-          <label htmlFor="bar">bar</label>
-          <input tyor='text' onClick= { () => {console.log('aaaaaaa');} } />
-        </React.Fragment>
-      );
-    }
+
+const App = () => {
+
+  const profiles = [
+    { name: 'Taro' , age : 10 },
+    { name: 'Ziro' , age : 8 },
+    { name: 'Hanako'},
+    { name: 'Daiki' , age : 8 },
+    { name: 'Yamato' },
+  ];
+
+  return (
+    <div>
+      {
+        profiles.map((profile , index) => {
+          return <User name={profile.name} age={profile.age} key={index}/>;
+        })
+      }
+    </div>
+  );
 };
+
+const User = props => {
+  return <div>HI , I am {props.name} and {props.age} years old </div>;
+};
+
+//属性の値が指定されていない場合、下記の値が代入される。
+User.defaultProps = {
+  age :1 
+};
+
+export default App ;
