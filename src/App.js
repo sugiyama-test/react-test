205febed55688bfe85a199c7c3febf171f94dc7f
respawn_sugiyama <sugiyama@respawn.tokyo>
2019-09-13 15:30:46 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+//このimportは絶対必要！！！
+//この構文が無いと、JSXをトランスパイルする処理が走らなくなる！！
+//逆を言えばJSXを使わなければ必要ない
 import React from 'react';
 
-function App() {
-  return <div><h1>hello</h1></div>;
-}
 
-export default App;
+//classはJSの予約語なのでJSXではclassNameと書く
+//属性名等は全てキャメルケースでかく。
+//React.FragmentはHTML上でみると表示されなくなるタグ。JSXで記述するときは親タグをつけなくてはいけないので、それをHTML上に表示させないようにするタグ。
+export default class App extends React.Component {
+  render() {
+      return (
+        <React.Fragment>
+          <label htmlFor="bar">bar</label>
+          <input tyor='text' onClick= { () => {console.log('aaaaaaa');} } />
+        </React.Fragment>
+      );
+    }
+};
